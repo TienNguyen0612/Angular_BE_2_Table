efdf9c940fd9ed4740ea392502beabc71daad921
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-03-29 23:31:24 +0700

    BE 2 Table

---
+++ src/main/java/codegym/repository/ICategoryRepository.java
@@ -0,0 +1,11 @@
+package codegym.repository;
+
+import codegym.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
+}
